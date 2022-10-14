ba2b9b1b9de34cabc1979ac2cefaa8adc5180c28
Rico <grico484@gmail.com>
2022-10-14 13:46:46 -0400

    Adjusting responsiveness on the home page

--- assets/home/index.js
+++ assets/home/index.js
@@ -1,11 +1,11 @@
 let toggle = document.getElementById('toggle');
 let botaoMenu = document.querySelector(".menu-hamburger");
-let menuLateral = document.querySelector(".menu-lateral");
+let menuLateral = document.querySelector(".side-menu");
 
 toggle.onclick = function(){
     toggle.classList.toggle('active');
 }
 
 botaoMenu.addEventListener('click', ()=> {
-    menuLateral.classList.toggle('menu-lateral-ativo');
+    menuLateral.classList.toggle('side-menu-ativo');
 });

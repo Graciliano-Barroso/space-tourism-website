ac746430580876c54b927bd0100a9f63224d1a64
Rico <grico484@gmail.com>
2022-10-16 21:42:45 -0400

    Poject finished

--- assets/home/index.js
+++ assets/home/index.js
@@ -1,11 +1,13 @@
+
 let toggle = document.getElementById('toggle');
-let botaoMenu = document.querySelector(".menu-hamburger");
-let menuLateral = document.querySelector(".side-menu");
+let buttonMenu = document.querySelector(".menu-hamburger");
+let sideMenu = document.querySelector(".side-menu");
+
 
 toggle.onclick = function(){
     toggle.classList.toggle('active');
 }
 
-botaoMenu.addEventListener('click', ()=> {
-    menuLateral.classList.toggle('side-menu-ativo');
+buttonMenu.addEventListener('click', ()=> {
+    sideMenu.classList.toggle('side-menu-ativo');
 });
